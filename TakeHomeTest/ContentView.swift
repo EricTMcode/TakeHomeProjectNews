adf703ee4a3d902ecbd80a482d3c9d28d13cacfc
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-07-10 15:39:13 +0200

    Add searchable

--- TakeHomeTest/ContentView.swift
+++ TakeHomeTest/ContentView.swift
@@ -12,15 +12,27 @@ struct ContentView: View {
 
     var body: some View {
         NavigationStack {
-            List(viewModel.articles, rowContent: ArticleRow.init)
-//            List(viewModel.articles) { article in
-//                ArticleRow(article: article)
-//            }
-            .navigationTitle("News")
-            .navigationDestination(for: Article.self, destination: ArticleView.init)
-//            .navigationDestination(for: Article.self) { article in
-//                ArticleView(article: article)
-//            }
+            switch viewModel.loadState {
+            case .failed:
+                LoadFailedView(error: viewModel.loadError, retry: viewModel.loadArticles)
+            default:
+                if viewModel.articles.isEmpty {
+                    ProgressView("Loading...")
+                        .controlSize(.extraLarge)
+                } else {
+                    List(viewModel.filteredArticles, rowContent: ArticleRow.init)
+                    //            List(viewModel.articles) { article in
+                    //                ArticleRow(article: article)
+                    //            }
+                        .navigationTitle("News")
+                        .navigationDestination(for: Article.self, destination: ArticleView.init)
+                        .refreshable(action: viewModel.loadArticles)
+                        .searchable(text: $viewModel.filterText, prompt: "Filter articles")
+                    //            .navigationDestination(for: Article.self) { article in
+                    //                ArticleView(article: article)
+                    //            }
+                }
+            }
         }
         .task(viewModel.loadArticles)
     }
