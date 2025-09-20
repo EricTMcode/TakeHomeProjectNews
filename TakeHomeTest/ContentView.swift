6c6fc2dd051257ea4f9af3905fc071ec3ae523ca
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-09-20 22:11:42 +0200

    Restart project

--- TakeHomeTest/ContentView.swift
+++ TakeHomeTest/ContentView.swift
@@ -8,33 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var viewModel = ViewModel()
-
     var body: some View {
-        NavigationStack {
-            switch viewModel.loadState {
-            case .failed:
-                LoadFailedView(error: viewModel.loadError, retry: viewModel.loadArticles)
-            default:
-                if viewModel.articles.isEmpty {
-                    ProgressView("Loading...")
-                        .controlSize(.extraLarge)
-                } else {
-                    List(viewModel.filteredArticles, rowContent: ArticleRow.init)
-                    //            List(viewModel.articles) { article in
-                    //                ArticleRow(article: article)
-                    //            }
-                        .navigationTitle("News")
-                        .navigationDestination(for: Article.self, destination: ArticleView.init)
-                        .refreshable(action: viewModel.loadArticles)
-                        .searchable(text: $viewModel.filterText, prompt: "Filter articles")
-                    //            .navigationDestination(for: Article.self) { article in
-                    //                ArticleView(article: article)
-                    //            }
-                }
-            }
+        VStack {
+            Text("Hello, World!")
         }
-        .task(viewModel.loadArticles)
     }
 }
 
