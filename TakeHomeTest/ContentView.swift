53cf27caccf6d931f6d14b3938ae3a00bb533213
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-09-22 16:06:46 +0200

    Create LoadFailedview

--- TakeHomeTest/ContentView.swift
+++ TakeHomeTest/ContentView.swift
@@ -14,19 +14,8 @@ struct ContentView: View {
         NavigationStack {
             switch viewModel.loadstate {
             case .failed:
-                ContentUnavailableView {
-                    Text("Load Error")
-                        .font(.headline)
-                } description: {
-                    Text("There was an error loading the articles.")
-                } actions: {
-                    Button("Retry") {
-                        Task {
-                            await viewModel.loadArticles()
-                        }
-                    }
-                }
-                
+                LoadFailedView(error: viewModel.loadError, retry: viewModel.loadArticles)
+
             default:
                 if viewModel.articles.isEmpty {
                     ProgressView("Loading..")
