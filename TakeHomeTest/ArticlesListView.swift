8f5c4e33594bb7b7bbfe168e71d1427ad3c84312
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-01-27 22:06:36 +0100

    Extract AsyncImage

--- TakeHomeTest/ArticlesListView.swift
+++ TakeHomeTest/ArticlesListView.swift
@@ -12,35 +12,9 @@ struct ArticlesListView: View {
 
     var body: some View {
         NavigationStack {
-            List(viewModel.articles) { article in
-                NavigationLink(value: article) {
-                    HStack {
-                        AsyncImage(url: article.thumbnail) { phase in
-                            switch phase {
-                            case .empty:
-                                ProgressView()
-                            case .success(let image):
-                                image
-                                    .resizable()
-                                    .scaledToFill()
-                            default:
-                                Image(systemName: "newspaper")
-                            }
-                        }
-                        .frame(width: 80, height: 80)
-                        .clipShape(.rect(cornerRadius: 10))
-
-                        VStack(alignment: .leading) {
-                            Text(article.section)
-                                .font(.caption.weight(.heavy))
-
-                            Text(article.title)
-                        }
-                    }
-                }
-            }
-            .navigationTitle("Take Home Test")
-            .navigationDestination(for: Article.self, destination: ArticleView.init)
+            List(viewModel.articles, rowContent: ArticleRow.init)
+                .navigationTitle("Take Home Test")
+                .navigationDestination(for: Article.self, destination: ArticleView.init)
         }
         .task(viewModel.loadArticles)
     }
