1b149b6d70f94fa6868bcf3d871da79b6846295e
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-01-28 22:53:25 +0100

    Add Filtering tests

--- TakeHomeTestTests/TakeHomeTestsTests.swift
+++ TakeHomeTestTests/TakeHomeTestsTests.swift
@@ -25,4 +25,27 @@ struct TakeHomeTestsTests {
         #expect(viewModel.articles.isEmpty == false, "There should be article after loading")
         #expect(viewModel.loadState == .loaded, "The view model should finish loading in the loaded state.")
     }
+
+    @Test func viewModelFilteringFull() async throws {
+        let viewModel = ArticlesListView.ViewModel()
+        await viewModel.loadArticles()
+
+        #expect(viewModel.filteredArticles == viewModel.articles, "Without a filter attached, all articles should be shown")
+    }
+
+    @Test func viewModelFilteringExact() async throws {
+        let viewModel = ArticlesListView.ViewModel()
+        await viewModel.loadArticles()
+        viewModel.filterText = viewModel.articles.first?.title ?? ""
+
+        #expect(viewModel.filteredArticles.count == 1, "Filtering for an exact article should only show 1 result.")
+    }
+
+    @Test func viewModelFilteringEmpty() async throws {
+        let viewModel = ArticlesListView.ViewModel()
+        await viewModel.loadArticles()
+        viewModel.filterText = "XXX NOT HAPPENING XXX"
+
+        #expect(viewModel.filteredArticles.isEmpty, "With an impossible article title, no articles shoud be shawn")
+    }
 }
